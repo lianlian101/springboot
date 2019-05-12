df7dddab48ab2af6e57ae303a071db82291f1c9e
admin <admin@192.168.0.100>
2019-05-12 12:09:41 +0800

    测试

---
+++ src/test/java/com/test/springboot/demo/ClassTest.java
@@ -0,0 +1,28 @@
+package com.test.springboot.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ClassTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    public void demo1() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/test"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+        //.andExpect(MockMvcResultMatchers.content().string("365"));  //测试接口返回内容
+    }
+    
+}
