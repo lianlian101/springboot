6f9508b41f50af16f9ebbea219e996ed8d6a6ba6
zhb <zhb@DESKTOP-R7KVT9O>
2019-08-22 11:09:26 +0800

    jstl测试

--- src/test/java/com/test/springboot/demo/controller/ClassTest.java
+++ src/test/java/com/test/springboot/demo/controller/ClassTest.java
@@ -1,4 +1,4 @@
-package com.test.springboot.demo;
+package com.test.springboot.demo.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- src/test/java/com/test/springboot/demo/test/Demo.java
+++ src/test/java/com/test/springboot/demo/test/Demo.java
@@ -1,4 +1,4 @@
-package com.test.springboot.demo;
+package com.test.springboot.demo.test;
 
 import java.util.HashMap;
 import java.util.Map;
