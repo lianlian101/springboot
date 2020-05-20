9b173731a8d3c9edf20f055ca041b4f907210ff5
zhb <zhb@DESKTOP-R7KVT9O>
2020-05-20 11:07:58 +0800

    获取网络地址测试

---
+++ src/main/java/com/test/springboot/general/util/AddressUtils.java
@@ -0,0 +1,134 @@
+package com.test.springboot.general.util;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AddressUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(AddressUtils.class);
+    private static final String LOCALHOST_IP = "127.0.0.1";
+    private static final String EMPTY_IP = "0.0.0.0";
+    private static final Pattern IP_PATTERN = Pattern.compile("[0-9]{1,3}(\\.[0-9]{1,3}){3,}");
+
+    public static boolean isAvailablePort(int port) {
+        ServerSocket ss = null;
+        try {
+            ss = new ServerSocket(port);
+            ss.bind(null);
+            return true;
+        } catch (IOException e) {
+            return false;
+        } finally {
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    private static boolean isValidHostAddress(InetAddress address) {
+        if (address == null || address.isLoopbackAddress())
+            return false;
+        String name = address.getHostAddress();
+        return (name != null && !EMPTY_IP.equals(name) && !LOCALHOST_IP.equals(name)
+                && IP_PATTERN.matcher(name).matches());
+    }
+
+    public static String getHostIp() {
+        InetAddress address = getHostAddress();
+        return address == null ? null : address.getHostAddress();
+    }
+
+    public static String getHostName() {
+        InetAddress address = getHostAddress();
+        return address == null ? null : address.getHostName();
+    }
+
+    public static InetAddress getHostAddress() {
+        InetAddress localAddress = null;
+        try {
+            localAddress = InetAddress.getLocalHost();
+            if (isValidHostAddress(localAddress)) {
+                return localAddress;
+            }
+        } catch (Throwable e) {
+            logger.warn("Failed to retriving local host ip address, try scan network card ip address. cause: "
+                    + e.getMessage());
+        }
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            if (interfaces != null) {
+                while (interfaces.hasMoreElements()) {
+                    try {
+                        NetworkInterface network = interfaces.nextElement();
+                        Enumeration<InetAddress> addresses = network.getInetAddresses();
+                        if (addresses != null) {
+                            while (addresses.hasMoreElements()) {
+                                try {
+                                    InetAddress address = addresses.nextElement();
+                                    if (isValidHostAddress(address)) {
+                                        return address;
+                                    }
+                                } catch (Throwable e) {
+                                    logger.warn("Failed to retriving network card ip address. cause:" + e.getMessage());
+                                }
+                            }
+                        }
+                    } catch (Throwable e) {
+                        logger.warn("Failed to retriving network card ip address. cause:" + e.getMessage());
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            logger.warn("Failed to retriving network card ip address. cause:" + e.getMessage());
+        }
+        logger.error("Could not get local host ip address, will use 127.0.0.1 instead.");
+        return localAddress;
+    }
+
+    public static String getRealIP() {
+        try {
+            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = allNetInterfaces.nextElement();
+
+                // 去除回环接口，子接口，未运行和接口
+                if (netInterface.isLoopback() || netInterface.isVirtual() || !netInterface.isUp()) {
+                    continue;
+                }
+
+                if (!netInterface.getDisplayName().contains("Intel")
+                        && !netInterface.getDisplayName().contains("Realtek")) {
+                    continue;
+                }
+                Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+                System.out.println(netInterface.getDisplayName());
+                while (addresses.hasMoreElements()) {
+                    InetAddress ip = addresses.nextElement();
+                    if (ip != null) {
+                        // ipv4
+                        if (ip instanceof Inet4Address) {
+                            System.out.println("ipv4 = " + ip.getHostAddress());
+                            return ip.getHostAddress();
+                        }
+                    }
+                }
+                //break;
+            }
+        } catch (SocketException e) {
+            System.err.println("Error when getting host ip address" + e.getMessage());
+        }
+        return null;
+    }
+}
